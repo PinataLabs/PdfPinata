647246b82dfadcbc0099e6460be913fde0fa4cd1
Jason Finch <jason.finch@gmail.com>
2026-09-26 13:47:37 +1000

    refactor: dereference through PdfReference.Dereference in the forms
    Five sites in PdfAcroField, PdfCheckBoxField and PdfChoiceField followed
    a reference by hand.
    
    Refs #157

--- src/PdfPinata/Pdf.AcroForms/PdfAcroField.cs
+++ src/PdfPinata/Pdf.AcroForms/PdfAcroField.cs
@@ -222,9 +222,7 @@ public abstract class PdfAcroField : PdfDictionary
     /// </summary>
     private IEnumerable<PdfDictionary> KidDictionaries()
     {
-        var item = Elements[Keys.Kids];
-        if (item is PdfReference reference)
-            item = reference.Value;
+        var item = PdfReference.Dereference(Elements[Keys.Kids]);
         if (item is not PdfArray kids)
             yield break;
 
@@ -890,9 +888,7 @@ public abstract class PdfAcroField : PdfDictionary
     {
         get
         {
-            var item = Elements[Keys.Kids];
-            if (item is PdfReference reference)
-                item = reference.Value;
+            var item = PdfReference.Dereference(Elements[Keys.Kids]);
 
             return item is PdfArray array && array.Elements.Count > 0;
         }
